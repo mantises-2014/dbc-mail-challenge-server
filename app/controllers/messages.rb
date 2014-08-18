8fb4b58428af3b504530b91e1bd7a4a25d8b9c71
Nate Delage <nate@natedelage.com>
2014-08-18 10:10:44 -0500

    DRY up before filters for /messages

--- app/controllers/messages.rb
+++ app/controllers/messages.rb
@@ -1,8 +1,12 @@
-before "/api/:email/messages" do
+before "/api/:email/messages*" do
   halt 403, "Invalid token" if api_token.nil?
-  api_token.record_request!
 
-  halt 429, "Too many requests" if api_token.throttled?
+  # /messages/count requests are exempt from rate limiting
+  if request.path_info.match(/count$/)
+    api_token.record_request!
+    halt 429, "Too many requests" if api_token.throttled?
+  end
+
   halt 502, "Server error due to white whale impalement." if Random.rand(20) == 19
   halt 404, "Email address not found." if EmailAddress.find_by(email_address: params[:email]).nil?
 end
@@ -12,12 +16,6 @@ get "/api/:email/messages" do
 
   content_type :xml
   email_address.messages.order("id desc").limit(50).to_xml
-end
-
-before "/api/:email/messages/count" do
-  halt 403, "Invalid token" if api_token.nil?
-  halt 502, "Server error due to white whale impalement." if Random.rand(20) == 19
-  halt 404, "Email address not found." if EmailAddress.find_by(email_address: params[:email]).nil?
 end
 
 get "/api/:email/messages/count" do
